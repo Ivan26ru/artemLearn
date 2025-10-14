2fcc766b8a96a77fe1b197f80407926763630ae5
xraid <xraider@list.ru>
2025-10-15 00:38:27 +0500

    Простой массив

--- back/index.php
+++ back/index.php
@@ -303,5 +303,15 @@ var_dump((bool) array()); // bool (true)
 var_dump((bool) "false"); // bool (false)
 
 
+$array1 = array(
+    "foo" => "bar",
+    "bar" => "foo",
+);
+$array2 = [
+    "foo" => "bar",
+    "bar" => "foo",
+];
+var_dump($array1, $array2);
+
 
 ?>
