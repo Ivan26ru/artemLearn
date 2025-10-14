2c38785c069a35da941e677314732897c111d3dd
xraid <xraider@list.ru>
2025-10-14 23:39:54 +0500

    Пример недопустимого смещения строки

--- back/index.php
+++ back/index.php
@@ -260,6 +260,19 @@ $srt[strlen($str) - 1] = 'e';
 var_dump($srt);
 // изменение последнего символа строки
 
+$str = 'abc';
+$keys = ['1', '1.0', 'x', '1x' ];
+
+foreach ($keys as $keyToTry) {
+    var_dump(isset($str[$keyToTry]));
+    try {
+        var_dump(isset($str[$keyToTry]));
+    } catch (TypeError $e) {
+        echo $e->getMessage(), PHP_EOL;
+    }
+    echo PHP_EOL;
+}
+
 
 
 
