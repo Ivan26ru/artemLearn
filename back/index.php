a7790b1367f44da5853d0523e053cc50087a9f39
xraid <xraider@list.ru>
2025-10-16 01:22:50 +0500

    Вызов языковой конструкции array()

--- back/index.php
+++ back/index.php
@@ -567,6 +567,29 @@ $b[] = 'c';
 
 var_dump($a, $b);
 
+$map = array(
+    'version' => 4,
+    'OS' => 'Linux',
+    'lang' => 'english',
+    'short_tags' => true
+);
+var_dump($map);
+
+$switching = array(
+    10,
+    5 => 6,
+    3 => 7,
+    'a' => 4,
+    11,
+    '8' => 2,
+    '02' => 77,
+    0 => 12
+);
+var_dump($switching);
+
+$empty = array();
+var_dump($empty);
+
 
 
 
