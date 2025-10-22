827b10b78dc03ae5acedd5da33fb0b117122ff9c
xraid <xraider@list.ru>
2025-10-22 22:35:04 +0500

    Основные переменные

---
+++ back/vars.php
@@ -0,0 +1,17 @@
+<?php
+echo "Переменные";
+$varInt = 123;
+$varString = "Эта строка";
+$varFloat = 12.3;
+$varBool = true;
+$varArray = [1, 2, 3];
+$varArrayKey = ['key 1' => 'value 1', 'key 2' => 'value 2'];
+$varNull = null;
+
+var_dump($varInt);
+var_dump($varString);
+var_dump($varFloat);
+var_dump($varBool);
+var_dump($varArray);
+var_dump($varArrayKey);
+var_dump($varNull);
