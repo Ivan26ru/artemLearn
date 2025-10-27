f1f39cbc883d5a05ba1823c91d56fb566bae4209
xraid <xraider@list.ru>
2025-10-27 22:49:23 +0500

    Убрать не используемый код

--- back/foreach.php
+++ back/foreach.php
@@ -1,6 +1,7 @@
 <?php
 
 $array = [1, 2, 3, 17];
+
 foreach ($array as $value) {
     echo "Значение текущего элемента массива \$array: $value.\n";
 }
@@ -11,6 +12,7 @@ $array = [
     "three" => 3,
     "seventeen" => 17
 ];
+
 foreach ($array as $key => $value) {
     echo "Ключ: $key =>Значение: $value.\n";
 }
@@ -27,61 +29,3 @@ foreach ($grid as $y => $row) {
     }
 }
 
-foreach (range(1, 10) as $value) {
-    echo "$value\n";
-}
-
-$array = [
-    [1, 2],
-    [3, 4],
-];
-
-foreach ($array as [$a, $b]) {
-    echo "A: $a; B: $b\n";
-}
-foreach ($array as list($a, $b)) {
-    echo "A: $a; B: $b\n";
-}
-
-$array = [
-    [1, 2, 5],
-    [3, 4, 6],
-];
-
-foreach ($array as [$a, $b]) {
-    // переменная $c не указана
-    echo "$a $b\n";
-}
-foreach ($array as [, , $c]) {
-    // пропущено $a и $b
-    echo "$c\n";
-}
-
-$array = [
-    [1, 2],
-    [3, 4],
-];
-foreach ($array as [$a, $b, $c]) {
-    echo "A: $a; B: $b; C: $c\n";
-}
-
-$arr = [1, 2, 3, 4];
-foreach ($arr as &$value) {
-    $value = $value * 2;
-}
-
-unset($value);
-
-$arr = [1, 2, 3, 4];
-foreach ($arr as &$value) {
-    $value = $value * 2;
-}
-
-foreach ($arr as $key => $value) {
-    echo "{$key} => {$value} ";
-    print_r($arr);
-}
-
-foreach ([1, 2, 3, 4] as &$value) {
-    $value = $value * 2;
-}
