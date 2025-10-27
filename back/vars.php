256ac25500b5ff68cb414b2fd056abb4ec61e9d7
xraid <xraider@list.ru>
2025-10-27 22:39:24 +0500

    Добавить многомерные массивы

--- back/vars.php
+++ back/vars.php
@@ -4,8 +4,13 @@ $varInt = 123;
 $varString = "Эта строка";
 $varFloat = 12.3;
 $varBool = true;
-$varArray = [1, 2, 3];
+$varArray = [10, 20, 30];
 $varArrayKey = ['key 1' => 'value 1', 'key 2' => 'value 2'];
+$varArrayInclude = [
+    'element 1' => [
+        'key 11' => 'value 11'
+    ],
+];
 $varNull = null;
 
 var_dump($varInt);
@@ -15,3 +20,6 @@ var_dump($varBool);
 var_dump($varArray);
 var_dump($varArrayKey);
 var_dump($varNull);
+var_dump($varArray[2]);
+var_dump($varArrayKey['key 1']);
+var_dump($varArrayInclude['element 1']['key 11']);
