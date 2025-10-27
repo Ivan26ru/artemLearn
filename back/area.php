2163b25caa5357261b6d88b62f1046190752dbe1
xraid <xraider@list.ru>
2025-10-27 23:03:33 +0500

    Оптимизировать функцию площади прямоугольника

--- back/area.php
+++ back/area.php
@@ -1,31 +1,13 @@
 <?php
 
-$a = 4;
-$b = 8;
-if ($a * $b)
-    $c = $a * $b;
-    echo "Длина = $b\n <br>";
-    echo "Ширина = $a\n <br>";
-    echo "$a * $b = $c\n <br>";
-    echo "S = $c\n <br>";
-
-
-
 
 function calculateArea($width, $length)
-    {
-    return ($width * $length);
-    }
-$area = calculateArea(12, 9);
-echo "S = $area\n <br>";
+{
+    $S = $width * $length;
+    echo "Ширина=" . $width . "<br>";
+    echo "Высота=" . $length . "<br>";
+    echo "Площадь=" . $S . "<br>";
+}
 
-
-// Это не функция, пробовал как пример
-$array = [
-    [4, 6, 24]
-];
-foreach ($array as [$a, $b, $c]);
-echo "Длина = $a\n <br>";
-echo "Ширина = $b\n <br>";
-echo "$a * $b = $c\n <br>";
-echo "Площадь = $c\n <br>";
+calculateArea(2, 4);
+calculateArea(9, 5);
