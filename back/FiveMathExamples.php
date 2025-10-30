4fc39bc1368df5d3c44f12281a72a6c168dd7727
xraid <xraider@list.ru>
2025-10-30 21:23:11 +0500

    Сделать сумму геометрический прогрессии

--- back/FiveMathExamples.php
+++ back/FiveMathExamples.php
@@ -8,3 +8,14 @@ function calculateWay($speed, $time)
     echo "Путь=$Way км<br>";
 }
 calculateWay(60, 12 );
+
+// сумма геометрическое прогрессии
+function calculateSGM($b, $q)
+{
+    $SGM = $b * 1 - $b * $q / 1-$q;
+    echo "b=$b<br>";
+    echo "q=$q <br>";
+    echo "SGM=$SGM <br>";
+}
+calculateSGM(40,50);
+
