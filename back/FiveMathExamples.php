38f9d0d07f6fe456f1c87e8e123c4f30b3dddce7
xraid <xraider@list.ru>
2025-11-04 19:42:36 +0500

    Исправить конкатенацию

--- back/FiveMathExamples.php
+++ back/FiveMathExamples.php
@@ -4,48 +4,53 @@ echo '$a';
 function calculateWay($speed, $time)
 {
     $way = $speed * $time;
-    echo "Скорость= ". $speed . "км/ч<br>";
+    echo "Скорость= " . $speed . "км/ч<br>";
     echo "Время= " . $time . "ч<br>";
     echo "Путь= " . $way . "км<br>";
 }
-calculateWay(60, 12 );
+
+calculateWay(60, 12);
 
 // сумма геометрическое прогрессии
 function calculateSumGeometricProgression($b, $q)
 {
-    $SGP = $b * 1 - $b * $q / 1-$q;
+    $SGP = $b * 1 - $b * $q / 1 - $q;
     echo "b=" . $b . "<br>";
     echo "q= " . $q . "<br>";
     echo "SGM= " . $SGP . "<br>";
 }
-calculateSumGeometricProgression(40,50);
+
+calculateSumGeometricProgression(40, 50);
 
 // куб разности
 function calculateCubeDifference($a, $b)
 {
-    $CD=$a*$a*$a-3*$a*$a*$b+3*$a*$b*$b-$b*$b*$b;
-    echo "a=$a <br>";
-    echo "b=$b <br>";
-    echo "CD=$CD <br>";
+    $CD = $a * $a * $a - 3 * $a * $a * $b + 3 * $a * $b * $b - $b * $b * $b;
+    echo "a=" . $a . "<br>";
+    echo "b=" . $b . "<br>";
+    echo "CD=" . $CD . "<br>";
 }
+
 calculateCubeDifference(8, 6);
 
 // Тождество
 function calculateIdentity($a, $b)
 {
     $identity = $a * $a + 2 * $a * $b + $b * $b;
-    echo "a=$a <br>";
-    echo "b=$b <br>";
-    echo "(a+b)^2=$identity <br>";
+    echo "a=" . $a . "<br>";
+    echo "b=" . $b . "<br>";
+    echo "(a+b)^2=" . $identity . "<br>";
 }
+
 calculateIdentity(3, 5);
 
 // Средняя линия трапеции
 function calculateMiddleLineTrapezoid($a, $b)
 {
     $l = $a + $b / 2;
-    echo "a=$a <br>";
-    echo "b=$b <br>";
-    echo "l=$l <br>";
+    echo "a=" . $a . "<br>";
+    echo "b=" . $b . "<br>";
+    echo "l=" . $l . "<br>";
 }
+
 calculateMiddleLineTrapezoid(4, 8);
