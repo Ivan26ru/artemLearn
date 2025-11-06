6c6611bad4fe827297b618877c18245cfc8571af
xraid <xraider@list.ru>
2025-11-07 01:43:57 +0500

    Реформат кода

--- back/equation2.php
+++ back/equation2.php
@@ -7,27 +7,28 @@ function discriminant($a, $b, $c)
     echo "D = $D <br>";
     return $D;
 }
-function calculateDiscriminant($a, $b, $c,)
+
+function calculateDiscriminant($a, $b, $c)
 {
     $D = discriminant($a, $b, $c);
     if ($D < 0) {
-        echo "d меньше 0 <br>";
+        echo "D меньше 0 <br>";
         echo "Действительных корней нет<br>";
-    } elseif ($D == 0){
-        echo "d равно 0 <br>";
+    } elseif ($D == 0) {
+        echo "D равно 0 <br>";
         $x1 = -$b / 2 * $a;
         echo "x1=" . $x1 . "<br>";
     } else {
-        echo "d больше 0<br>";
+        echo "D больше 0<br>";
         $x1 = -$b + sqrt($D) / 2 * $a;
         $x2 = -$b - sqrt($D) / 2 * $a;
-        echo "x1=". $x1 . "<br>";
+        echo "x1=" . $x1 . "<br>";
         echo "x2=" . $x2 . "<br>";
     }
-echo "<br>";
+    echo "<br>";
 
 }
 
-calculateDiscriminant(1,2 ,1 );
-calculateDiscriminant(4,1 ,1 );
-calculateDiscriminant(1,4 ,1 );
+calculateDiscriminant(1, 2, 1);
+calculateDiscriminant(4, 1, 1);
+calculateDiscriminant(1, 4, 1);
