4572d773b817ae46b25f0380af78bf7e50db3643
xraid <xraider@list.ru>
2025-11-09 23:04:32 +0500

    Реализовать черновой вариант валидации значений дискриминанта

--- back/get.php
+++ back/get.php
@@ -1,45 +1,44 @@
 <form action="#" method="get">
-    <input name="a" placeholder="Значение A">
-    <input name="b" placeholder="Значение B">
-    <input name="c" placeholder="Значение C">
+    <input name="a" placeholder="Значение A"
+            <?php if (!validateFieldByKey('a')) { ?>
+                style="border-color:red"
+            <?php } ?>
+    >
+    <input name="b" placeholder="Значение B"
+            <?php if (!validateFieldByKey('b')) { ?>
+                style="border-color:red"
+            <?php } ?>
+    >
+    <input name="c" placeholder="Значение C"
+            <?php if (!validateFieldByKey('c')) { ?>
+                style="border-color:red"
+            <?php } ?>
+    >
+    <input name="hasCalculated" value="1">
     <button type="submit">Отправить</button>
+    <!--    <button type="reset">Сбросить</button>-->
+    <a href="/back/get.php">Сбросить</a>
 </form>
 
 <?php
 
-//https://student/back/get.php?a=аппельсин&b=55
-//array(2) { ["a"]=> string(18) "аппельсин" ["b"]=> string(2) "55" }
-if (isset($_GET['a']) && isset($_GET['b']) && isset($_GET['c'])) {
-    echo "Значение а,b,c что-то считаем<br>";
+function validateFieldByKey($key): bool
+{
+    if (isset($_GET['hasCalculated'])) {
+        return isset($_GET[$key]) && !empty($_GET[$key]);
+    } else {
+        return true;
+    }
+}
+
+if (!isset($_GET['a'])) {
+    echo 'А не заполнено<br>';
+}
+
+if (isset($_GET['hasCalculated']) && validateFieldByKey('a') && validateFieldByKey('b') && validateFieldByKey('c')) {
     calculateDiscriminant($_GET['a'], $_GET['b'], $_GET['c']);
-} else {
-    echo '<label> Поле «Значений A,B,C» не должно быть пустым  <br> </label> <style> label{ color: red; } input{border: 1px solid red}</style>';
 }
-if (isset($_GET['a'])){
-    echo '<label>Поле a, пустое<br></label> <style> label{ color: red; } input:nth-child(1){border: 1px solid red}</style>';
-}
-if (isset($_GET['b'])){
-    echo '<label>Поле b, пустое<br></label> <style> label{ color: red; } input:nth-child(2){border: 1px solid red}</style>';
-}
-if (isset($_GET['c'])){
-    echo '<label>Поле c, пустое<br></label> <style> label{ color: red; } input:nth-child(3){border: 1px solid red}</style>';
-}
-//if ($a == null) {
-//    echo '<label> Поле «Значений A,B,C» не должно быть пустым  <br> </label> <style> label{ color: red; } input{border: 1px solid red}</style>';
-//} elseif ($b == null) {
-//    echo '<label> Поле «Значений A,B,C» не должно быть пустым  <br> </label> <style> label{ color: red; } input{border: 1px solid red}</style>';
-//} elseif ($c == null) {
-//    echo '<label> Поле «Значений A,B,C» не должно быть пустым  <br> </label> <style> label{ color: red; } input{border: 1px solid red}</style>';
-//}
-//if (!isset($_GET['a'])) {
-//    echo '<label> Поле «Значений A» не должно быть пустым  <br> </label> <style> label{ color: red; } input:nth-child(1){border: 1px solid red}</style>';
-//}
-//if ($b != 0 ){
-//    echo '<label> Поле «Значений B» не должно быть пустым  <br> </label> <style> label{ color: red; } input:nth-child(2){border: 1px solid red}</style>';
-//}
 var_dump($_GET);
-
-
 
 
 function discriminant($a, $b, $c)
