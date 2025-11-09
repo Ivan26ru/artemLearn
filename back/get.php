92f0dd6099012f4e001416e81d8113bb6d050860
Ivan <it-1ivan@ya.ru>
2025-11-09 23:52:38 +0500

    Ivan исправленный код получения дискриминанта с подставлением значения в input

--- back/get.php
+++ back/get.php
@@ -1,73 +1,96 @@
 <form action="#" method="get">
-    <input name="a" placeholder="Значение A"
-            <?php if (!validateFieldByKey('a')) { ?>
-                style="border-color:red"
-            <?php } ?>
-    >
-    <input name="b" placeholder="Значение B"
-            <?php if (!validateFieldByKey('b')) { ?>
-                style="border-color:red"
-            <?php } ?>
-    >
-    <input name="c" placeholder="Значение C"
-            <?php if (!validateFieldByKey('c')) { ?>
-                style="border-color:red"
-            <?php } ?>
-    >
-    <input name="hasCalculated" value="1">
-    <button type="submit">Отправить</button>
-    <!--    <button type="reset">Сбросить</button>-->
-    <a href="/back/get.php">Сбросить</a>
+	<input name="a" placeholder="Значение A"
+			<?php if (checkCalculatedWork() && !validateFieldByKey('a')) { ?>
+				style="border-color:red"
+			<?php } ?>
+			value="<?php echo findFieldByKey('a') ?>">
+
+	<input name="b" placeholder="Значение B"
+			<?php if (checkCalculatedWork() && !validateFieldByKey('b')) { ?>
+				style="border-color:red"
+			<?php } ?>
+			value="<?php echo findFieldByKey('b') ?>">
+
+	<input name="c" placeholder="Значение C"
+			<?php if (checkCalculatedWork() && !validateFieldByKey('c')) { ?>
+				style="border-color:red"
+			<?php } ?>
+
+			value="<?php echo findFieldByKey('c') ?>">
+
+	<input name="hasCalculated" value="1">
+	<button type="submit">Отправить</button>
+	<!--    <button type="reset">Сбросить</button>-->
+	<a href="?">Сбросить</a>
 </form>
 
+<?php if (checkCalculatedWork()) {
+	echo "Результаты подсчета ниже";
+} else {
+	echo "Для подсчета нажмите отправить";
+} ?>
+
+<hr>
+
 <?php
+function findFieldByKey($key): string
+{
+	if (isset($_GET[$key])) {
+		return $_GET[$key];
+	} else {
+		return "";
+	}
+}
 
 function validateFieldByKey($key): bool
 {
-    if (isset($_GET['hasCalculated'])) {
-        return isset($_GET[$key]) && !empty($_GET[$key]);
-    } else {
-        return true;
-    }
+	return isset($_GET[$key]) && !empty($_GET[$key]);
 }
 
-if (!isset($_GET['a'])) {
-    echo 'А не заполнено<br>';
+function checkCalculatedWork(): bool
+{
+	return isset($_GET['hasCalculated']);
 }
 
-if (isset($_GET['hasCalculated']) && validateFieldByKey('a') && validateFieldByKey('b') && validateFieldByKey('c')) {
-    calculateDiscriminant($_GET['a'], $_GET['b'], $_GET['c']);
+if (
+		checkCalculatedWork()
+		&& validateFieldByKey('a')
+		&& validateFieldByKey('b')
+		&& validateFieldByKey('c')
+) {
+	calculateDiscriminant($_GET['a'], $_GET['b'], $_GET['c']);
 }
+
 var_dump($_GET);
 
 
 function discriminant($a, $b, $c)
 {
-    $D = $b * $b - 4 * $a * $c;
-    echo "D = $D <br>";
-    return $D;
+	$D = $b * $b - 4 * $a * $c;
+	echo "D = $D <br>";
+	return $D;
 }
 
 function calculateDiscriminant($a, $b, $c)
 {
-    echo 'a=' . $a . "<br>";
-    echo 'b=' . $b . "<br>";
-    echo 'c=' . $c . "<br>";
-    $D = discriminant($a, $b, $c);
-    if ($D < 0) {
-        echo "D меньше 0 <br>";
-        echo "Действительных корней нет<br>";
-    } elseif ($D == 0) {
-        echo "D равно 0 <br>";
-        $x1 = -$b / 2 * $a;
-        echo "x1=" . $x1 . "<br>";
-    } else {
-        echo "D больше 0<br>";
-        $x1 = -$b + sqrt($D) / 2 * $a;
-        $x2 = -$b - sqrt($D) / 2 * $a;
-        echo "x1=" . $x1 . "<br>";
-        echo "x2=" . $x2 . "<br>";
-    }
-    echo "<br>";
+	echo 'a=' . $a . "<br>";
+	echo 'b=' . $b . "<br>";
+	echo 'c=' . $c . "<br>";
+	$D = discriminant($a, $b, $c);
+	if ($D < 0) {
+		echo "D меньше 0 <br>";
+		echo "Действительных корней нет<br>";
+	} elseif ($D == 0) {
+		echo "D равно 0 <br>";
+		$x1 = -$b / 2 * $a;
+		echo "x1=" . $x1 . "<br>";
+	} else {
+		echo "D больше 0<br>";
+		$x1 = -$b + sqrt($D) / 2 * $a;
+		$x2 = -$b - sqrt($D) / 2 * $a;
+		echo "x1=" . $x1 . "<br>";
+		echo "x2=" . $x2 . "<br>";
+	}
+	echo "<br>";
 
 }
