df38da9dd784535fc3de7194104701c3d8acc6c6
xraid <xraider@list.ru>
2025-11-14 22:15:36 +0500

    Добавил типизацию

--- back/exampleIf.php
+++ back/exampleIf.php
@@ -9,7 +9,7 @@ $testGet = [
 var_dump(getValueByKey($_GET,'a'));// 1
 var_dump(getValueByKey($_GET, 'b'));// false
 var_dump(getValueByKey($_GET, 'c'));// false
-function getValueByKey($currentArray, $key)
+function getValueByKey(array $currentArray, string $key): string | false
 {
 
     if (!isset($currentArray[$key]) || empty($currentArray[$key])) {
