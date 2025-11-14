bd8518e9bf48107e0a890f14d3cab315d59f63cb
xraid <xraider@list.ru>
2025-11-14 21:57:02 +0500

    Функция, которая получает значения из массива по ключу, если его нет возвращает false

---
+++ back/exampleIf.php
@@ -0,0 +1,20 @@
+<?php
+$testGet = [
+    'a' => 1,
+    'b' => '',
+    // c - отсутствует
+];
+
+
+var_dump(getValueByKey($_GET,'a'));// 1
+var_dump(getValueByKey($_GET, 'b'));// false
+var_dump(getValueByKey($_GET, 'c'));// false
+function getValueByKey($currentArray, $key)
+{
+
+    if (!isset($currentArray[$key]) || empty($currentArray[$key])) {
+        return false;
+    }
+
+    return $currentArray[$key];
+}
