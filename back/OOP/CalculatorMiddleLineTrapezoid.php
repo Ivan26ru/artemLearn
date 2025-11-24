ab1544bf6160075ea9d064ca40a5c0630f529440
xraid <xraider@list.ru>
2025-11-25 01:17:07 +0500

    Создать класс калькулятора средней линии трапеции

---
+++ back/OOP/CalculatorMiddleLineTrapezoid.php
@@ -0,0 +1,29 @@
+<?php
+
+class CalculatorMiddleLineTrapezoid
+{
+    private int $a;
+    private int $b;
+    public function __construct(int $a, int $b)
+    {
+        $this->a = $a;
+        $this->b = $b;
+    }
+    public function getA(): int
+    {
+        return $this->a;
+    }
+    public function getB(): int
+    {
+        return $this->b;
+    }
+    public function calculateMiddleLineTrapezoid() : string
+    {
+        $l = $this->a + $this->b / 2;
+        return "Средняя линии трапеции=" . $l;
+    }
+}
+$l= new CalculatorMiddleLineTrapezoid(8,2);
+echo "A=" . $l->getA() . "<br>";
+echo "B=" . $l->getB() . "<br>";
+echo $l->calculateMiddleLineTrapezoid();
